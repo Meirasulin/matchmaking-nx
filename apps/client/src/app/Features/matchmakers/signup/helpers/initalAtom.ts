0db9330b4977724a0460961b823eeede84dbc1e1
Meirasulin <asulinmeir64@gmail.com>
2024-01-18 09:06:13 +0200

    some updates

--- apps/client/src/app/Features/matchmakers/signup/helpers/initalAtom.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { TypeUserInfo, TypeContactInfo, TypePersonalDetails } from "../types/userInfoType";
-
-export const stepAtom = atom(1)
-
-export const userInfoAtomMatchmaker = atom<null | TypeUserInfo | TypeContactInfo | TypePersonalDetails>(null)
